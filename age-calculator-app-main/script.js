189eba035a73f7e968d5d5e3a5200ef996258753
ChanduTelaga <202217b3191@wilp.bits-pilani.ac.in>
2026-07-12 17:38:14 +0530

    Started working on js file and added few varibles in js and made few changes to html and css

---
+++ age-calculator-app-main/script.js
@@ -0,0 +1,9 @@
+const now = new Date();
+const inpDay = document.getElementById("day");
+const inpMonth = document.getElementById("month");
+const inpYear = document.getElementById("year")
+
+const years = document.getElementById("npYears");
+const months = document.getElementById("npMonths");
+const days = document.getElementById("npDays");
+
